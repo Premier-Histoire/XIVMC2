e4e3f19919ecc0e4fcdb112f7fdcd7a4dd6f7afe
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-03-20 23:55:57 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import MobileApp from "./MobileApp.vue";
 import 'bootstrap/dist/css/bootstrap.css'
 
-createApp(App).mount('#app')
+createApp(window.innerWidth <= 600 ? MobileApp : App).mount('#app')
