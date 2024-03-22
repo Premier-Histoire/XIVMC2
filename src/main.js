efe82b9516fe1ca6f4fe76b5eed860227d42376a
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-03-23 00:43:01 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import MobileApp from './MobileApp.vue';
 import 'bootstrap/dist/css/bootstrap.css'
 
 createApp(App).mount('#app')
